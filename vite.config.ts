363a02a3a7098d0d26675ae9f3461eb61121a776
Zhang Jiancheng <zhangjiancheng2021@outlook.com>
2024-06-15 09:06:40 +0800

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import VueDevTools from 'vite-plugin-vue-devtools'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    VueDevTools(),
+    AutoImport({ imports: ['vue', 'vue-router'], resolvers: [NaiveUiResolver()] }),
+    Components({ resolvers: [NaiveUiResolver()] })
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
